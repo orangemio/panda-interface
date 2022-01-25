26ba62a5a7e0392c7cfda8d3a8cde9c9e01775f3
vdovaT <vdova@protonmail.com>
2022-01-25 23:27:37 +0800

    update

--- src/constants/dist/lists.js
+++ src/constants/dist/lists.js
@@ -1,22 +1,22 @@
-"use strict";
-exports.__esModule = true;
-exports.DEFAULT_LIST_OF_LISTS = exports.DEFAULT_TOKEN_LIST_URL = void 0;
+'use strict'
+exports.__esModule = true
+exports.DEFAULT_LIST_OF_LISTS = exports.DEFAULT_TOKEN_LIST_URL = void 0
 // the Uniswap Default token list lives here
-exports.DEFAULT_TOKEN_LIST_URL = window.location.origin + '/tokens.json';
+exports.DEFAULT_TOKEN_LIST_URL = 'https://tokens.pancakeswap.finance/pancakeswap-extended.json'
 exports.DEFAULT_LIST_OF_LISTS = [
-    exports.DEFAULT_TOKEN_LIST_URL
-    // 't2crtokens.eth', // kleros
-    // 'tokens.1inch.eth' // 1inch
-    // 'synths.snx.eth',
-    // 'tokenlist.dharma.eth',
-    // 'defi.cmc.eth',
-    // 'erc20.cmc.eth',
-    // 'stablecoin.cmc.eth',
-    // 'tokenlist.zerion.eth',
-    // 'tokenlist.aave.eth',
-    // 'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json',
-    // 'https://app.tryroll.com/tokens.json',
-    // 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
-    // 'https://defiprime.com/defiprime.tokenlist.json',
-    // 'https://umaproject.org/uma.tokenlist.json'
-];
+  exports.DEFAULT_TOKEN_LIST_URL
+  // 't2crtokens.eth', // kleros
+  // 'tokens.1inch.eth' // 1inch
+  // 'synths.snx.eth',
+  // 'tokenlist.dharma.eth',
+  // 'defi.cmc.eth',
+  // 'erc20.cmc.eth',
+  // 'stablecoin.cmc.eth',
+  // 'tokenlist.zerion.eth',
+  // 'tokenlist.aave.eth',
+  // 'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json',
+  // 'https://app.tryroll.com/tokens.json',
+  // 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
+  // 'https://defiprime.com/defiprime.tokenlist.json',
+  // 'https://umaproject.org/uma.tokenlist.json'
+]
